86c8adc229d31a5b598e8d41da4e069d60481cab
Georges <geomia2@hotmail.com>
2023-02-03 21:47:39 +0200

    commit android side of the login and sign up

---
+++ app/src/main/java/com/example/login_register/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.login_register;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position==1){
+            return new RegisterFragment();
+        }
+        else{
+            LoginFragment login = new LoginFragment();
+
+            return login;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
